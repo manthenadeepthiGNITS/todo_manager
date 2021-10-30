f58439e644a8e95da9708cf36d165882cde59c02
manthenadeepthiGNITS <manthena.deepthi@gmail.com>
2021-10-30 12:34:32 +0530

    formatted erb files and deleted unnecessary methods and comments

--- app/models/todo.rb
+++ app/models/todo.rb
@@ -1,18 +1,6 @@
 class Todo < ActiveRecord::Base
-    def to_pleasant_string
-        iscompleted=completed ? "[X]" :"[ ]"
-        "#{id} #{todo_text} #{iscompleted}"
-    end
-    def due_today?
-        due_date == Date.today
-    end
-    def workdone
-        completed
-    end
-
     def self.overdue
-        #where("due_date < ? and completed = ", Date.today,"false")
-        where("completed = ? and due_date < ?", false, Date.today)
+       where("completed = ? and due_date < ?", false, Date.today)
     end
     def self.due_today
         where("due_date = ?", Date.today)
@@ -22,19 +10,5 @@ class Todo < ActiveRecord::Base
     end
     def self.completed
         all.where(completed: true)
-    end
-
-    def self.show_list
-        puts "My Todo-list\n\n"
-        puts "Overdue\n"
-        puts overdue.map { |todo| todo.to_displayable_string }
-        puts "\n\n"
-        puts "Due Today\n"
-        puts due_today.map { |todo| todo.to_displayable_string }
-        puts "\n\n"
-
-        puts "Due Later\n"
-        puts due_later.map { |todo| todo.to_displayable_string }
-        puts "\n\n"
     end
 end
