2d36031c12f98fa66b439a5948649972c96b1c07
manthenadeepthiGNITS <manthena.deepthi@gmail.com>
2021-11-05 10:29:10 +0530

    fully functional todo app

--- app/controllers/todos_controller.rb
+++ app/controllers/todos_controller.rb
@@ -1,23 +1,17 @@
 class TodosController < ApplicationController
-  skip_before_action :verify_authenticity_token
     def index
-      #render plain: Todo.order(:due_date).map{
-      #  |todo| todo.to_pleasant_string
-      #  }.join("\n")
       render "index"
     end
     def show
       id=params[:id]
       todo=Todo.find(id)
       render "todo"
-      #render plain:todo.to_pleasant_string
     end
     def create
       todo_text=params[:todo_text]
       due_date=DateTime.parse(params[:due_date])
       new_todo=Todo.create!(todo_text:todo_text,due_date:due_date,completed:false)
       response_text="creation done #{new_todo.id}"
-      #render plain:response_text
       redirect_to todos_path
     end
     def update
